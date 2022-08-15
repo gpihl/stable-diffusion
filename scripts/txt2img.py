d533cd6ab8d9f7c60cb89938ed32c426408240cd
Gustaf Pihl <gpihl@kth.se>
2022-08-15 20:29:33 +0000

    stuff

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -1,243 +1,168 @@
 import argparse, os, sys, glob
-import torch
 import numpy as np
-from omegaconf import OmegaConf
+import time
+import torch
 from PIL import Image
 from tqdm import tqdm, trange
 from itertools import islice
 from einops import rearrange
 from torchvision.utils import make_grid
-import time
 from pytorch_lightning import seed_everything
 from torch import autocast
 from contextlib import contextmanager, nullcontext
-
-from ldm.util import instantiate_from_config
 from ldm.models.diffusion.ddim import DDIMSampler
 from ldm.models.diffusion.plms import PLMSSampler
 
+# def chunk(it, size):
+#     it = iter(it)
+#     return iter(lambda: tuple(islice(it, size)), ())
 
-def chunk(it, size):
-    it = iter(it)
-    return iter(lambda: tuple(islice(it, size)), ())
-
-
-def load_model_from_config(config, ckpt, verbose=False):
-    print(f"Loading model from {ckpt}")
-    pl_sd = torch.load(ckpt, map_location="cpu")
-    if "global_step" in pl_sd:
-        print(f"Global Step: {pl_sd['global_step']}")
-    sd = pl_sd["state_dict"]
-    model = instantiate_from_config(config.model)
-    m, u = model.load_state_dict(sd, strict=False)
-    if len(m) > 0 and verbose:
-        print("missing keys:")
-        print(m)
-    if len(u) > 0 and verbose:
-        print("unexpected keys:")
-        print(u)
-
-    model.cuda()
-    model.eval()
-    return model
-
-
-def main():
+def main(prompt, batch_size, seed, model):
     parser = argparse.ArgumentParser()
 
-    parser.add_argument(
-        "--prompt",
+    parser.add_argument("--prompt",
         type=str,
         nargs="?",
         default="a painting of a virus monster playing guitar",
         help="the prompt to render"
     )
-    parser.add_argument(
-        "--outdir",
+    parser.add_argument("--outdir",
         type=str,
         nargs="?",
         help="dir to write results to",
         default="outputs/txt2img-samples"
     )
-    parser.add_argument(
-        "--skip_grid",
+    parser.add_argument("--skip_grid",
         action='store_true',
         help="do not save a grid, only individual samples. Helpful when evaluating lots of samples",
     )
-    parser.add_argument(
-        "--skip_save",
+    parser.add_argument("--skip_save",
         action='store_true',
         help="do not save individual samples. For speed measurements.",
     )
-    parser.add_argument(
-        "--ddim_steps",
+    parser.add_argument("--ddim_steps",
         type=int,
         default=50,
         help="number of ddim sampling steps",
     )
-    parser.add_argument(
-        "--plms",
+    parser.add_argument("--plms",
         action='store_true',
         help="use plms sampling",
     )
-    parser.add_argument(
-        "--laion400m",
+    parser.add_argument("--laion400m",
         action='store_true',
+        default=True,
         help="uses the LAION400M model",
     )
-    parser.add_argument(
-        "--fixed_code",
+    parser.add_argument("--fixed_code",
         action='store_true',
         help="if enabled, uses the same starting code across samples ",
     )
-    parser.add_argument(
-        "--ddim_eta",
+    parser.add_argument("--ddim_eta",
         type=float,
         default=0.0,
         help="ddim eta (eta=0.0 corresponds to deterministic sampling",
     )
-    parser.add_argument(
-        "--n_iter",
+    parser.add_argument("--n_iter",
         type=int,
-        default=2,
+        default=1,
         help="sample this often",
     )
-    parser.add_argument(
-        "--H",
+    parser.add_argument("--H",
         type=int,
         default=512,
         help="image height, in pixel space",
     )
-    parser.add_argument(
-        "--W",
+    parser.add_argument("--W",
         type=int,
         default=512,
         help="image width, in pixel space",
     )
-    parser.add_argument(
-        "--C",
+    parser.add_argument("--C",
         type=int,
         default=4,
         help="latent channels",
     )
-    parser.add_argument(
-        "--f",
+    parser.add_argument("--f",
         type=int,
         default=8,
         help="downsampling factor",
     )
-    parser.add_argument(
-        "--n_samples",
+    parser.add_argument("--n_samples",
         type=int,
         default=3,
         help="how many samples to produce for each given prompt. A.k.a. batch size",
     )
-    parser.add_argument(
-        "--n_rows",
+    parser.add_argument("--n_rows",
         type=int,
         default=0,
         help="rows in the grid (default: n_samples)",
     )
-    parser.add_argument(
-        "--scale",
+    parser.add_argument("--scale",
         type=float,
         default=7.5,
         help="unconditional guidance scale: eps = eps(x, empty) + scale * (eps(x, cond) - eps(x, empty))",
     )
-    parser.add_argument(
-        "--from-file",
+    parser.add_argument("--from-file",
         type=str,
         help="if specified, load prompts from this file",
     )
-    parser.add_argument(
-        "--config",
+    parser.add_argument("--config",
         type=str,
         default="configs/stable-diffusion/v1-inference.yaml",
         help="path to config which constructs model",
     )
-    parser.add_argument(
-        "--ckpt",
+    parser.add_argument("--ckpt",
         type=str,
         default="models/ldm/stable-diffusion-v1/model.ckpt",
         help="path to checkpoint of model",
     )
-    parser.add_argument(
-        "--seed",
+    parser.add_argument("--seed",
         type=int,
         default=42,
         help="the seed (for reproducible sampling)",
     )
-    parser.add_argument(
-        "--precision",
+    parser.add_argument("--precision",
         type=str,
         help="evaluate at this precision",
         choices=["full", "autocast"],
         default="autocast"
     )
+    parser.add_argument("8890"
+    )
+    
     opt = parser.parse_args()
-
-    if opt.laion400m:
-        print("Falling back to LAION 400M model...")
-        opt.config = "configs/latent-diffusion/txt2img-1p4B-eval.yaml"
-        opt.ckpt = "models/ldm/text2img-large/model.ckpt"
-        opt.outdir = "outputs/txt2img-samples-laion400m"
-
-    seed_everything(opt.seed)
-
-    config = OmegaConf.load(f"{opt.config}")
-    model = load_model_from_config(config, f"{opt.ckpt}")
-
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    model = model.to(device)
+    seed_everything(seed)
 
     if opt.plms:
         sampler = PLMSSampler(model)
     else:
         sampler = DDIMSampler(model)
 
-    os.makedirs(opt.outdir, exist_ok=True)
-    outpath = opt.outdir
-
-    batch_size = opt.n_samples
-    n_rows = opt.n_rows if opt.n_rows > 0 else batch_size
-    if not opt.from_file:
-        prompt = opt.prompt
-        assert prompt is not None
-        data = [batch_size * [prompt]]
-
-    else:
-        print(f"reading prompts from {opt.from_file}")
-        with open(opt.from_file, "r") as f:
-            data = f.read().splitlines()
-            data = list(chunk(data, batch_size))
-
-    sample_path = os.path.join(outpath, "samples")
-    os.makedirs(sample_path, exist_ok=True)
-    base_count = len(os.listdir(sample_path))
-    grid_count = len(os.listdir(outpath)) - 1
+    data = [batch_size * [prompt]]
 
     start_code = None
     if opt.fixed_code:
-        start_code = torch.randn([opt.n_samples, opt.C, opt.H // opt.f, opt.W // opt.f], device=device)
+        start_code = torch.randn([batch_size, opt.C, opt.H // opt.f, opt.W // opt.f], device=device)
 
+    images = []
+        
     precision_scope = autocast if opt.precision=="autocast" else nullcontext
     with torch.no_grad():
         with precision_scope("cuda"):
             with model.ema_scope():
-                tic = time.time()
-                all_samples = list()
                 for n in trange(opt.n_iter, desc="Sampling"):
                     for prompts in tqdm(data, desc="data"):
                         uc = None
                         if opt.scale != 1.0:
                             uc = model.get_learned_conditioning(batch_size * [""])
-                        if isinstance(prompts, tuple):
-                            prompts = list(prompts)
+                        # if isinstance(prompts, tuple):
+                        #     prompts = list(prompts)
                         c = model.get_learned_conditioning(prompts)
                         shape = [opt.C, opt.H // opt.f, opt.W // opt.f]
                         samples_ddim, _ = sampler.sample(S=opt.ddim_steps,
                                                          conditioning=c,
-                                                         batch_size=opt.n_samples,
+                                                         batch_size=batch_size,
                                                          shape=shape,
                                                          verbose=False,
                                                          unconditional_guidance_scale=opt.scale,
@@ -247,33 +172,11 @@ def main():
 
                         x_samples_ddim = model.decode_first_stage(samples_ddim)
                         x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
+                        for x_sample in x_samples_ddim:
+                            x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
+                            images.append(Image.fromarray(x_sample.astype(np.uint8)))
 
-                        if not opt.skip_save:
-                            for x_sample in x_samples_ddim:
-                                x_sample = 255. * rearrange(x_sample.cpu().numpy(), 'c h w -> h w c')
-                                Image.fromarray(x_sample.astype(np.uint8)).save(
-                                    os.path.join(sample_path, f"{base_count:05}.png"))
-                                base_count += 1
-
-                        if not opt.skip_grid:
-                            all_samples.append(x_samples_ddim)
-
-                if not opt.skip_grid:
-                    # additionally, save as grid
-                    grid = torch.stack(all_samples, 0)
-                    grid = rearrange(grid, 'n b c h w -> (n b) c h w')
-                    grid = make_grid(grid, nrow=n_rows)
-
-                    # to image
-                    grid = 255. * rearrange(grid, 'c h w -> h w c').cpu().numpy()
-                    Image.fromarray(grid.astype(np.uint8)).save(os.path.join(outpath, f'grid-{grid_count:04}.png'))
-                    grid_count += 1
-
-                toc = time.time()
-
-    print(f"Your samples are ready and waiting for you here: \n{outpath} \n"
+    print(f"Your samples are ready \n"
           f" \nEnjoy.")
-
-
-if __name__ == "__main__":
-    main()
+    
+    return images
---
+++ server.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
+import sys
+import base64
+import json
+from time import sleep
+from PIL import Image
+from io import BytesIO
+from urllib.parse import parse_qs
+import scripts.txt2img as txt2img
+from omegaconf import OmegaConf
+import torch
+from ldm.util import instantiate_from_config
+
+def init_model():
+    config = OmegaConf.load("configs/latent-diffusion/txt2img-1p4B-eval.yaml")
+    model = load_model_from_config(config, "models/ldm/text2img-large/model.ckpt")
+
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    model = model.to(device)
+    
+    return model
+
+def load_model_from_config(config, ckpt, verbose=False):
+    print(f"Loading model from {ckpt}")
+    pl_sd = torch.load(ckpt, map_location="cpu")
+    if "global_step" in pl_sd:
+        print(f"Global Step: {pl_sd['global_step']}")
+    sd = pl_sd["state_dict"]
+    model = instantiate_from_config(config.model)
+    m, u = model.load_state_dict(sd, strict=False)
+    if len(m) > 0 and verbose:
+        print("missing keys:")
+        print(m)
+    if len(u) > 0 and verbose:
+        print("unexpected keys:")
+        print(u)
+
+    model.cuda()
+    model.eval()
+    return model
+
+def cypher(prompt):
+    res = ''
+    for c in prompt:
+        new_c = chr((ord(c) - 97 + 10) % 26 + 97)
+        res += new_c
+    return res
+
+def decypher(prompt):
+    res = ''
+    for c in prompt:
+        if c == ' ':
+            res += ' '
+            continue
+            
+        ascii_val = ord(c) - 97
+        new_ascii_val = ascii_val - 10
+        if new_ascii_val < 0:
+            new_ascii_val += 26
+
+        new_c = chr(new_ascii_val + 97)
+        res += new_c
+    return res
+
+class S(BaseHTTPRequestHandler):
+    def _set_post_response(self):
+        self.send_response(200)
+        self.send_header("Content-type", "application/json")
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+    
+    def _set_get_response(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()        
+
+    def do_GET(self):
+        self._set_get_response()
+        with open('image_gen.html', mode='r', encoding='utf-8') as html_file:
+            self.wfile.write(html_file.read().encode('utf-8'))
+
+    def do_POST(self):
+        try:
+            if self.path != '/generate':
+                return
+
+            server = self.server
+
+            if server.lock:
+                while server.lock:
+                    sleep(3)                                
+
+            server.lock = True
+            content_length = int(self.headers['Content-Length'])
+            post_data = self.rfile.read(content_length)
+            data = parse_qs(post_data.decode(), strict_parsing=True)
+            prompt = data['prompt'][0]
+            num_imgs = int(data['numImgs'][0])
+            seed = int(data['seed'][0])
+            cyph = data['cy'][0] == 'true'
+            if cyph:
+                prompt = decypher(prompt)
+
+            print('running txt2img with prompt')
+            images = txt2img.main(prompt, num_imgs, seed, server.model)        
+            resp = []
+            for img in images:
+                buffered = BytesIO()
+                img.save(buffered, 'png')
+                img_str = base64.b64encode(buffered.getvalue()).decode('utf-8')
+                resp.append(img_str)
+
+            self._set_post_response()
+            resp = json.dumps(resp)
+            self.wfile.write(resp.encode('utf-8'))
+            server.lock = False
+   
+        except Exception as e:
+            print(e)
+            server.lock = False
+
+def run(server_class=HTTPServer, handler_class=S, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    httpd.lock = True
+    model = init_model()
+    httpd.model = model
+    httpd.lock = False
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print('Closing server')
+
+if __name__ == '__main__':
+    from sys import argv
+    print('Starting server')        
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
+
