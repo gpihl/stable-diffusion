4e2e95f28779f359353ac1ff657088900f1471b3
Gustaf Pihl <gustafpihl@gmail.com>
2022-08-19 07:50:24 +0000

    df

--- init_setup.sh
+++ init_setup.sh
@@ -9,4 +9,5 @@ echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIImiY+UKS6pJpQIC/Og261HwIZ9eQytmMj/vuo
 apt update &&
 apt install nano &&
 git config --global user.email "gpihl@kth.se" &&
-git config --global user.name "Gustaf Pihl"
+git config --global user.name "Gustaf Pihl" &&
+python server.py 8890
