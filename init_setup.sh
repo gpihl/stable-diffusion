9a77c24d66e755dfa96142e4fa5c4d3a90d4a92d
Gustaf Pihl <gpihl@kth.se>
2022-08-28 21:33:37 +0000

    sdfs

--- init_setup.sh
+++ init_setup.sh
@@ -5,7 +5,6 @@ mv /weights/sd.ckpt /workspace/stable-diffusion/models/ldm/stable-diffusion-v1/m
 conda init bash &&
 . /root/.bashrc &&
 conda activate /workspace/stable-diffusion/envs &&
-echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIImiY+UKS6pJpQIC/Og261HwIZ9eQytmMj/vuogZ2pnD gustafpihl@gmail.com' > /root/.ssh/authorized_keys &&
 apt update &&
 apt install nano &&
 git config --global user.email "g@g.g" &&
