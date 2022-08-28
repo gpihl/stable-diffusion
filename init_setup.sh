0d93ff2f0d2c97cd2a5f0830f5342343587040b1
Gustaf Pihl <gpihl@kth.se>
2022-08-28 19:50:01 +0000

    dfsdf

--- init_setup.sh
+++ init_setup.sh
@@ -8,6 +8,5 @@ conda activate /workspace/stable-diffusion/envs &&
 echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIImiY+UKS6pJpQIC/Og261HwIZ9eQytmMj/vuogZ2pnD gustafpihl@gmail.com' > /root/.ssh/authorized_keys &&
 apt update &&
 apt install nano &&
-git config --global user.email "gpihl@kth.se" &&
-git config --global user.name "Gustaf Pihl" &&
-python server.py 8890
+git config --global user.email "g@g.g" &&
+git config --global user.name "gpihl" &&
